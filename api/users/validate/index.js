94bd1c2265e91d70427d927b95a099e3cafbb280
Ghost <admin@itsghost.me>
2016-03-31 00:01:29 +0100

    HotFix

--- api/users/validate/index.js
+++ api/users/validate/index.js
@@ -2,7 +2,7 @@ var logger = require('./../../logger.js');
 
 module.exports = function (getMain, data, con, res) {
     var user = con.url.substring(con.url.lastIndexOf('/') + 1);
-    var exists = getMain().getSTO().list.map((obj) => {return obj.name}).contains(user);
+    var exists = getMain().getSTO().map((obj) => {return obj.name}).contains(user);
     
     if (exists) {
         res.ends(['In use'], 600, 'In use');
